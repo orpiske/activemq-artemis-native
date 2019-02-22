f528be892396ffa30f2876a3517511927758cfd8
Clebert Suconic <clebertsuconic@apache.org>
2019-02-22 11:53:57 -0500

    progress

--- compile-using-docker.sh
+++ compile-using-docker.sh
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-docker build -f ./docker/Dockerfile-Centos -t artemis-native-builder .
+docker build -f ./docker-build/Dockerfile-centos -t artemis-native-builder .
 
 #cmake .
 #make
